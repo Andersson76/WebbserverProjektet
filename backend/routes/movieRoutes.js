13feb75634b89e6088255688c85543fdd5184f06
Jonas Hultberg <jonas.hultberg@iths.se>
2024-04-25 13:30:37 +0200

    routing

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -4,8 +4,8 @@ const movieController = require("../controllers/movieController");
 
 router.get("/api/movies", movieController.getMovies);
 router.get("/api/movies/:id", movieController.getMovie);
-/* router.post("/api/books", bookController.createBook);
-router.put("/api/books", bookController.updateBook);
+router.post("/api/movies", movieController.createMovie);
+/*router.put("/api/books", bookController.updateBook);
 router.delete("/api/books", bookController.deleteBook);
 router.get("/api/books-categories", bookController.getBooksCategories); */
 
