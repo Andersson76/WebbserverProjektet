986f0f57c0bda6e5acdb716a6d6f574bfce44040
annekurvinen <149961248+annekurvinen@users.noreply.github.com>
2024-05-02 01:20:19 +0200

    writerController was updated,  writerRoutes.js was added and updated in app.js

--- backend/controllers/writerController.js
+++ backend/controllers/writerController.js
@@ -112,3 +112,22 @@ exports.updateWriter = async (req, res) => {
     });
   }
 };
+
+// Get movieWiter
+exports.getMovieWriter = async (req, res) => {
+  let sql =
+    "SELECT * FROM movies INNER JOIN writer ON movies.writerID = writer.writerID";
+
+  try {
+    await connectionMySQL.query(sql, (error, results, fields) => {
+      if (error) {
+        if (error) throw error;
+      }
+      res.json(results);
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
