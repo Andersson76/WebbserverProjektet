d3ed73adc35dfd44d263d1c8fe78b1bdc248b0ed
Jonas Hultberg <jonas.hultberg@iths.se>
2024-05-03 11:03:52 +0200

    genre

--- backend/controllers/genreController.js
+++ backend/controllers/genreController.js
@@ -1,4 +1,4 @@
-/*const connectionMySQL = require("../connectionMySQL");
+const connectionMySQL = require("../connectionMySQL");
 
 exports.getGenre = async (req, res) => {
   let sql = "SELECT * FROM genre";
@@ -15,4 +15,27 @@ exports.getGenre = async (req, res) => {
     });
   }
 };
-*/
+
+exports.updateGenre = async (req, res) => {
+  const { id, name } = req.body; // Anta att du skickar med id och namn för genren att uppdatera i req.body
+
+  if (!id || !name) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  let sql = "UPDATE genre SET name = ? WHERE id = ?";
+  let values = [name, id];
+
+  try {
+    await connectionMySQL.query(sql, values, (error, results, fields) => {
+      if (error) {
+        throw error;
+      }
+      res.json({ message: "Genre updated successfully" });
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
