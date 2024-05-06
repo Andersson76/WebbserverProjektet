618fc197f1ac88ad8ad7e6d4defb3880c79535ca
Jonas Hultberg <jonas.hultberg@iths.se>
2024-05-06 13:55:06 +0200

    test

--- frontend/src/components/Movies.vue
+++ frontend/src/components/Movies.vue
@@ -5,8 +5,14 @@
 
     <!-- Lista med filmer -->
     <div class="flex flex-col items-center">
-      <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <li v-for="movie in movies" :key="movie.movieId" class="bg-white rounded-lg shadow-md p-4">
+      <ul
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+      >
+        <li
+          v-for="movie in movies"
+          :key="movie.movieId"
+          class="bg-white rounded-lg shadow-md p-4"
+        >
           <h2 class="text-lg font-semibold">{{ movie.movieTitle }}</h2>
           <h2 class="text-lg font-semibold">{{ movie.movieGenreId }}</h2>
 
@@ -23,18 +29,32 @@
         <!-- Formulär för att lägga till en ny film -->
         <form @submit.prevent="addMovie" class="max-w-sm mx-auto mt-8">
           <div class="mb-4">
-            <label for="movieTitle" class="block text-gray-700">Filmtitel:</label>
-            <input type="text" id="movieTitle" v-model="movieTitle" required
-              class="mt-1 block w-full rounded-md border-gray-900 shadow-sm border-indigo-300 ring ring-indigo-200 ring-opacity-50" />
+            <label for="movieTitle" class="block text-gray-700"
+              >Filmtitel:</label
+            >
+            <input
+              type="text"
+              id="movieTitle"
+              v-model="movieTitle"
+              required
+              class="mt-1 block w-full rounded-md border-gray-900 shadow-sm border-indigo-300 ring ring-indigo-200 ring-opacity-50"
+            />
           </div>
           <div class="mb-4 border-gray-900">
             <label for="movieRating" class="block text-gray-700">Betyg:</label>
-            <input type="text" id="movieRating" v-model="movieRating" required
-              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm border-indigo-300 ring ring-indigo-200 ring-opacity-50" />
+            <input
+              type="text"
+              id="movieRating"
+              v-model="movieRating"
+              required
+              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm border-indigo-300 ring ring-indigo-200 ring-opacity-50"
+            />
           </div>
           <div class="mt-4">
-            <button type="submit"
-              class="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">
+            <button
+              type="submit"
+              class="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+            >
               Lägg till film
             </button>
           </div>
@@ -42,23 +62,44 @@
         <!-- Formulär för att uppdatera film -->
         <form @submit.prevent="updateMovie" class="max-w-sm mx-auto mt-8">
           <div class="mb-4">
-            <label for="updateMovieId" class="block text-gray-700">Film-ID:</label>
-            <input type="text" id="updateMovieId" v-model="updateMovieId" required
-              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm border-indigo-300 ring ring-indigo-200 ring-opacity-50" />
+            <label for="updateMovieId" class="block text-gray-700"
+              >Film-ID:</label
+            >
+            <input
+              type="text"
+              id="updateMovieId"
+              v-model="updateMovieId"
+              required
+              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm border-indigo-300 ring ring-indigo-200 ring-opacity-50"
+            />
           </div>
           <div class="mb-4">
-            <label for="movieTitle" class="block text-gray-700">Filmtitel:</label>
-            <input type="text" id="movieTitle" v-model="updateMovieTitle" required
-              class="mt-1 block w-full rounded-md border-gray-900 shadow-sm border-indigo-300 ring ring-indigo-200 ring-opacity-50" />
+            <label for="movieTitle" class="block text-gray-700"
+              >Filmtitel:</label
+            >
+            <input
+              type="text"
+              id="movieTitle"
+              v-model="updateMovieTitle"
+              required
+              class="mt-1 block w-full rounded-md border-gray-900 shadow-sm border-indigo-300 ring ring-indigo-200 ring-opacity-50"
+            />
           </div>
           <div class="mb-4 border-gray-900">
             <label for="movieRating" class="block text-gray-700">Betyg:</label>
-            <input type="text" id="movieRating" v-model="updateMovieRating" required
-              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm border-indigo-300 ring ring-indigo-200 ring-opacity-50" />
+            <input
+              type="text"
+              id="movieRating"
+              v-model="updateMovieRating"
+              required
+              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm border-indigo-300 ring ring-indigo-200 ring-opacity-50"
+            />
           </div>
           <div class="mt-4">
-            <button type="submit"
-              class="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">
+            <button
+              type="submit"
+              class="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+            >
               Uppdatera
             </button>
           </div>
@@ -75,6 +116,9 @@ const movies = ref([]);
 const movieTitle = ref("");
 const movieRating = ref("");
 const updateMovieId = ref("");
+//
+const updateMovieTitle = ref(""); // Här deklareras updateMovieTitle
+const updateMovieRating = ref(""); // Här deklareras updateMovieRating
 
 const fetchMovies = async () => {
   try {
@@ -124,12 +168,14 @@ const updateMovie = async () => {
       headers: {
         "Content-Type": "application/json",
       },
+
       body: JSON.stringify({
-        movieId: movieId.value,
-        movieTitle: movieTitle.value,
-        movieRating: movieRating.value,
+        movieId: updateMovieId.value,
+        movieTitle: updateMovieTitle.value,
+        movieRating: updateMovieRating.value,
       }),
     });
+    console.log(JSON.stringify("test"));
     const responseData = await response.json();
     if (responseData.success) {
       alert(responseData.message);
